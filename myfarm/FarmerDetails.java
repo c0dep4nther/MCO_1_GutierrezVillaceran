1673cf55b268e6aceab23fa9f9810330872da734
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2022-10-29 06:21:22 +0800

    initliaze seeds
    put info about seeds

--- myfarm/FarmerDetails.java
+++ myfarm/FarmerDetails.java
@@ -1,4 +1,20 @@
 package myfarm;
 
+import myfarm.board.Board;
+
+import java.util.ArrayList;
+
 public class FarmerDetails {
+    private Board playerBoard;
+    private ArrayList<Plants> plants;
+
+    public FarmerDetails(){
+        this.playerBoard=new Board();
+        this.plants=new ArrayList<Plants>();
+    }
+    public void addSeeds(String name, String type, float harvestTime, int waterNeeded, int waterBonus,
+                         int fertilizerNeeded, int fertilizerBonus, int harvestCost, int produceCountMax,
+                         int seedCost, int sellPrice) {
+            this.plants.add(new Plants(name,type,harvestTime,waterNeeded,waterBonus,fertilizerNeeded,fertilizerBonus,harvestCost,produceCountMax,seedCost,sellPrice));
+    }
 }
--- myfarm/Main.java
+++ myfarm/Main.java
@@ -4,7 +4,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to MyFarm!");
 
-        ;
-
+    }
+    //initializing player
+    private FarmerDetails farmer;
+    public  Main(){
+        farmer=new FarmerDetails();
+    }
+    //Initializing seed types
+    public void setupSeeds(){
+        farmer.addSeeds("Turnip","Vegetable",1,1,2,0,1,1,1,5,6);
+        farmer.addSeeds("Carrot","Vegetable",1.5f,1,2,0,1,1,2,10,9);
+        farmer.addSeeds("Rose","Flower",1,1,2,0,1,2,1,5,5);
+        farmer.addSeeds("Stargazer","Flower",2.5f,2,3,0,1,2,1,10,9);
     }
 }

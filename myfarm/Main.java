00c0ab153be3ab7c8471d25bdee6a1e1452deb3f
Mark Daniel Gutierrez <markdcg@outlook.com>
2022-10-29 21:23:12 +0800

    Accessing tile details made easier
    - add getTileStatus to get current state of Tile
    - crop choices now enclosed into a List

--- myfarm/Main.java
+++ myfarm/Main.java
@@ -1,22 +1,57 @@
 package myfarm;
 
+import myfarm.board.Board;
+import myfarm.board.Tile;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static final Scanner input = new Scanner(System.in);
+
+    // using a list to store the crops
+    public static final List<Plants> seedList = new ArrayList<>(Arrays.asList(
+            new Plants("Turnip", "Vegetable", 1f,
+                    1, 0, 1,
+                    1, 5, 6),
+            new Plants("Carrot", "Vegetable", 1.5f,
+                    1, 0, 1,
+                    2, 10, 9),
+            new Plants("Rose", "Flower", 2.5f,
+                    1, 0, 1,
+                    3, 15, 12),
+            new Plants("Stargazer", "Flower", 2.5f,
+                    2, 0, 2,
+                    1, 10, 9)
+    ));
+
     public static void main(String[] args) {
         System.out.println("Welcome to MyFarm!");
 
         /** CODE TO SHOW IN TERMINAL CROPS PLANTED*/
+        // instantiate the player
+        FarmerDetails player = new FarmerDetails();
+        // create a new board
+        Board gameBoard = new Board();
 
-    }
-    //initializing player
-    private FarmerDetails farmer;
-    public  Main(){
-        farmer=new FarmerDetails();
-    }
-    //Initializing seed types
-    public void setupSeeds(){
-        farmer.addSeeds("Turnip","Vegetable",1,1,2,0,1,1,1,5,6);
-        farmer.addSeeds("Carrot","Vegetable",1.5f,1,2,0,1,1,2,10,9);
-        farmer.addSeeds("Rose","Flower",1,1,2,0,1,2,1,5,5);
-        farmer.addSeeds("Stargazer","Flower",2.5f,2,3,0,1,2,1,10,9);
+        // print the board (key, value) in 10x5
+        for (int i = 1; i <= 50; i++) {
+            System.out.print(i + "\t[" + gameBoard.getTileStatus(i) + "]\t" );
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+        }
+
+        // ask player to choose a tile
+
+//        // loop through board and access the tile object
+//        for (int i = 1; i <= 50; i++) {
+//            // get the tile object
+//            Tile tile = gameBoard.getTiles().get(i);
+//        }
     }
 }
+
+// 1 - turnip, 2 carrot, 3 rose, 4 stargazer
--- myfarm/board/Tile.java
+++ myfarm/board/Tile.java
@@ -1,10 +1,14 @@
 package myfarm.board;
 
 public class Tile {
+    enum TileStatus {
+        UNPLOWED, PLANTED, WITHERED, HARVESTED
+    }
     private boolean isPlowed;
     private boolean isWithered;
     private boolean seedPlanted;
     private boolean harvestReady;
+    private TileStatus status;
 
     // tile constructor
     public Tile() {
@@ -12,6 +16,7 @@ public class Tile {
         isWithered = false;
         seedPlanted = false;
         harvestReady = false;
+        status = TileStatus.UNPLOWED;
     }
 
     // plow tile using the Plow class
@@ -19,5 +24,41 @@ public class Tile {
         isPlowed = true;
     }
 
+//    getters
+    public boolean isPlowed() {
+        return isPlowed;
+    }
 
+    public boolean isWithered() {
+        return isWithered;
+    }
+
+    public boolean isSeedPlanted() {
+        return seedPlanted;
+    }
+
+    public boolean isHarvestReady() {
+        return harvestReady;
+    }
+
+    public TileStatus getStatus() {
+        return status;
+    }
+
+//    setters
+    public void setPlowed(boolean plowed) {
+        isPlowed = plowed;
+    }
+
+    public void setWithered(boolean withered) {
+        isWithered = withered;
+    }
+
+    public void setSeedPlanted(boolean seedPlanted) {
+        this.seedPlanted = seedPlanted;
+    }
+
+    public void setHarvestReady(boolean harvestReady) {
+        this.harvestReady = harvestReady;
+    }
 }

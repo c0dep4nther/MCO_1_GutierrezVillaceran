089195e8bef9b4def488c8b470197a73234f458f
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2022-10-31 17:45:34 +0800

    Edited Farmerdetails

--- myfarm/FarmerDetails.java
+++ myfarm/FarmerDetails.java
@@ -5,42 +5,86 @@ import myfarm.board.Board;
 import java.util.ArrayList;
 
 public class FarmerDetails {
-    private Board playerBoard;
-    private ArrayList<Plants> plants;
-    static int seedCount=0;
-    static int fertilizerCount=5;
-    static int objectCoins=100;
-    static int farmerLevel=0;
-    static RegisterFarmer farmerStatus=RegisterFarmer.FARMER;
 
+    private int seedCount=0;
+    private int fertilizerCount=5;
+    private int objectCoins=100;
+    private int farmerLevel=0;
+    private RegisterFarmer farmerStatus=RegisterFarmer.FARMER;
+    private int experience=0;
 
-
-    public FarmerDetails(){
-        this.playerBoard=new Board();
-        this.plants=new ArrayList<Plants>();
-    }
-    public void addSeeds(String name, String type, float harvestTime, int waterNeeded, int waterBonus,
-                         int fertilizerNeeded, int fertilizerBonus, int harvestCost, int produceCountMax,
-                         int seedCost, int sellPrice) {
-            this.plants.add(new Plants(name,type,harvestTime, waterNeeded,waterBonus,fertilizerNeeded,
-                    fertilizerBonus,harvestCost,produceCountMax, seedCost,sellPrice));
-    }
-
-    public void AddWater(){
+    public void addWater(Tools tool){
         //code to water plants
+        objectCoins=objectCoins-tool.getCost();
     }
-    public void PlowLand(){
-        objectCoins=objectCoins-2;
+    public void plowLand(Tools tool){
+        objectCoins=objectCoins- tool.getCost();
         //code to plow land
     }
+    /**method to increase money and experience when harvesting*/
+    public void harvestPlant(Plants plant, int numOfHarvest){
+        int sellingPrice=0;
+        setExperience(experience+1); //increases experience when harvesting
 
-    public void Harvest(){
-        //code to harvest including increasing of Objectcoins
+        //NOT SURE SA COMPUTATION NG WATER, FERTILIZER, AND CROP BONUS
+        sellingPrice=farmerStatus.getBonusLimits()+plant.getSellPrice()+plant.getFertilizerBonus()+ plant.getWaterBonus();
+        setObjectCoins(objectCoins+sellingPrice*numOfHarvest);
+
     }
 
-    public void BuySeeds(){
-        //code to bawas objectcoins based on price of seed
-        //factor in status of farmer like bonus ganon
+    /**method to reduce money when buying seeds*/
+    public void buySeeds(Plants plant,int numberOfSeeds){
+        int newCost=plant.getSeedCost()-farmerStatus.getBuying();
+        setObjectCoins(getObjectCoins()-newCost*numberOfSeeds);
+        setSeedCount(seedCount+numberOfSeeds);
+    }
+
+    public int getSeedCount() {
+        return seedCount;
+    }
+
+    public void setSeedCount(int seedCount) {
+        this.seedCount = seedCount;
+    }
+
+    public int getFertilizerCount() {
+        return fertilizerCount;
+    }
+
+    public void setFertilizerCount(int fertilizerCount) {
+        this.fertilizerCount = fertilizerCount;
+    }
+
+    public int getObjectCoins() {
+        return objectCoins;
+    }
+
+    public void setObjectCoins(int objectCoins) {
+        this.objectCoins = objectCoins;
+    }
+
+    public int getFarmerLevel() {
+        return farmerLevel;
+    }
+
+    public void setFarmerLevel(int experience) {
+        this.farmerLevel = this.experience/100;
+    }
+
+    public RegisterFarmer getFarmerStatus() {
+        return farmerStatus;
+    }
+
+    public void setFarmerStatus(RegisterFarmer farmerStatus) {
+        this.farmerStatus = farmerStatus;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
     }
 
 
